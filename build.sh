1e5f3232b8088671b6d1eb781aa1df2a6bfa6728
Dai <yonglond@amazon.com>
2017-07-09 22:04:52 -0700

    refine build script

--- build.sh
+++ build.sh
@@ -8,23 +8,13 @@ SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 BUILD_DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
-# Derive the project name from the directory
-PROJECT="go_exe"
-
 # Setup the environment for the build
 export GOPATH=$GOPATH:$BUILD_DIR
-
-# echo "GOPATH" $GOPATH
-# echo "PATH" $PATH
-# echo "BUILD "$BUILD_DIR
-# echo "PROJECT " $PROJECT
-# echo "GOPATH " $GOPATH
-# echo "SOURCE " $SOURCE
 
 # Build the project
 cd $BUILD_DIR
 go clean
-go build -o ../bin/$PROJECT entry
+go build -o ../bin/entry entry
 go install entry
 
 EXIT_STATUS=$?
